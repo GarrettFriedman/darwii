9d6938b313f462b8aa9a694e03c90ce99e3e8531
GarrettFriedman <friedman.garrett@gmail.com>
2013-10-09 01:11:58 -0400

    functional primer calculator

--- www/docs-assets/js/primers.js
+++ www/docs-assets/js/primers.js
@@ -3,8 +3,11 @@ var ge = function(id){return document.getElementById(id);} //function to get ele
 
 var f = ge("FirstValue");
 var s = ge("SecondValue");
-var a = ge("Answer");
+var a1 = ge("Answer1");
+var a2 = ge("Answer2");
 
-a.value = f.value * s.value;
+a1.value = (100/((1000*(f.value / s.value))/5));
+a2.value = (((1000*(f.value / s.value))/5));
+
 
 }
